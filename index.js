10db6aff9db5d14250f3f7d10c86eca36a3b6b2a
Lukas Niemeier <lukas.niemeier@zalando.de>
2016-07-18 15:46:33 +0200

    logging

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.all('*', function(request, response) {
 
     console.log(request.body.payload);
 
-    var payload = JSON.parse(request.body.payload.replace(/\\"/g, '"'));
+    //var payload = JSON.parse(request.body.payload.replace(/\\"/g, '"'));
+    var payload = JSON.parse(request.body.payload);
 
     console.log(type + ' @ ' + room);
 
